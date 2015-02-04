423777990380f64c4b79f5f296f1666093a16085
Kyle Chadha <kyle.chadha@gmail.com>
2015-02-04 17:07:44 -0500

    Integrate negroni with mux

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	"github.com/codegangsta/negroni"
-	// "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 	"github.com/russross/blackfriday"
 )
 
@@ -17,34 +17,38 @@ func main() {
 		port = "8080"
 	}
 
+	// ROUTES
+	// -------
+	router := mux.NewRouter().StrictSlash(false)
+
+	// Posts Collection
+	posts := router.Path("/posts").Subrouter()
+	posts.Methods("GET").HandlerFunc(PostsIndexHandler)
+	posts.Methods("POST").HandlerFunc(PostsCreateHandler)
+
+	// Posts Singular
+	post := router.PathPrefix("/posts/{id}").Subrouter()
+	post.Methods("GET").Path("/edit").HandlerFunc(PostEditHandler)
+	post.Methods("GET").HandlerFunc(PostShowHandler)
+	post.Methods("PUT", "POST").HandlerFunc(PostUpdateHandler)
+	post.Methods("DELETE").HandlerFunc(PostDeleteHandler)
+
+	// Old Routes & Handlers
+	http.HandleFunc("/markdown", GenerateMarkdown)
+
+	// MIDDLEWARE
+	// -----------
 	n := negroni.New(
 		negroni.NewRecovery(),
 		negroni.HandlerFunc(AppMiddleware),
 		negroni.NewLogger(),
 		negroni.NewStatic(http.Dir("public")),
 	)
+	n.UseHandler(router)
 
-	// // r := mux.NewRouter().StrictSlash(false)
-	// r := mux.NewRouter()
-	// r.Handle("/", http.FileServer(http.Dir("public")))
-
-	// // Posts Collection
-	// posts := r.Path("/posts").Subrouter()
-	// posts.Methods("GET").HandlerFunc(PostsIndexHandler)
-	// posts.Methods("POST").HandlerFunc(PostsCreateHandler)
-
-	// // Posts Singular
-	// post := r.PathPrefix("/posts/{id}").Subrouter()
-	// post.Methods("GET").Path("/edit").HandlerFunc(PostEditHandler)
-	// post.Methods("GET").HandlerFunc(PostShowHandler)
-	// post.Methods("PUT", "POST").HandlerFunc(PostUpdateHandler)
-	// post.Methods("DELETE").HandlerFunc(PostDeleteHandler)
-
-	// fmt.Printf("The magic happens on port :%s\n", port)
+	// SERVER
+	// -------
 	n.Run(":" + port)
-
-	// Old Routes & Handlers
-	http.HandleFunc("/markdown", GenerateMarkdown)
 }
 
 func AppMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
@@ -63,30 +67,30 @@ func AppMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFun
 // 	fmt.Fprintln(rw, "Home")
 // }
 
-// func PostsIndexHandler(rw http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(rw, "posts index")
-// }
+func PostsIndexHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "posts index")
+}
 
-// func PostsCreateHandler(rw http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(rw, "posts create")
-// }
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "posts create")
+}
 
-// func PostShowHandler(rw http.ResponseWriter, r *http.Request) {
-// 	id := mux.Vars(r)["id"]
-// 	fmt.Fprintln(rw, "showing post", id)
-// }
+func PostShowHandler(rw http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintln(rw, "showing post", id)
+}
 
-// func PostUpdateHandler(rw http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(rw, "post update")
-// }
+func PostUpdateHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post update")
+}
 
-// func PostDeleteHandler(rw http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(rw, "post delete")
-// }
+func PostDeleteHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post delete")
+}
 
-// func PostEditHandler(rw http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(rw, "post edit")
-// }
+func PostEditHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post edit")
+}
 
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
 	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
