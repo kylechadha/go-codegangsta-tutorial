0922e7f7ba5f15970e858b116d125d7d21520063
Kyle Chadha <kyle.chadha@gmail.com>
2015-02-04 14:22:54 -0500

    Define routes

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/russross/blackfriday"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/russross/blackfriday"
 )
 
 func main() {
@@ -12,9 +14,27 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":"+port, nil)
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/", HomeHandler)
+
+	// Posts Collection
+	posts := r.Path("/posts").Subrouter()
+	posts.Methods("GET").HandlerFunc(PostsIndexHandler)
+	posts.Methods("POST").HandlerFunc(PostsCreateHandler)
+
+	// Posts Singular
+	post := r.PathPrefix("/posts/{id}").Subrouter()
+	post.Methods("GET").Path("/edit").HandlerFunc(PostEditHandler)
+	post.Methods("GET").HandlerFunc(PostShowHandler)
+	post.Methods("PUT", "POST").HandlerFunc(PostUpdateHandler)
+	post.Methods("DELETE").HandlerFunc(PostDeleteHandler)
+
+	fmt.Println("The magic happens on port :%s\n", port)
+	http.ListenAndServe(":"+port, r)
+
+	// http.HandleFunc("/markdown", GenerateMarkdown)
+	// http.Handle("/", http.FileServer(http.Dir("public")))
+	// http.ListenAndServe(":"+port, nil)
 }
 
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
